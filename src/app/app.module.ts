4876a113e91e479b34b099ff6afa9753894e6470
Marx Guimaraes <fidel.guimaraes@fpf.br>
2018-06-14 12:05:46 -0400

    Basic search and search results implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HTTP } from '@ionic-native/http';
 
 import { App } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -27,6 +29,7 @@ import { CardSearchPage } from '../pages/card-search/card-search';
   providers: [
     StatusBar,
     SplashScreen,
+    HTTP,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/card-search/card-search.ts
+++ src/pages/card-search/card-search.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { HTTP } from '@ionic-native/http';
 
 /**
  * Generated class for the CardSearchPage page.
@@ -13,12 +14,29 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'card-search.html',
 })
 export class CardSearchPage {
+  searchTerms: string;
+  searchResults: any[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: HTTP) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CardSearchPage');
+  }
+
+  basicSearch() {
+    this.http.get(`https://api.scryfall.com/cards/search`, {q: this.searchTerms}, {})
+      .then(res => {
+        let scryfallObj = JSON.parse(res.data);
+        this.searchResults = scryfallObj.data;
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }
+
+  clearSearch() {
+    this.searchResults = undefined;
   }
 
 }
